4bf0745a817c8472118f24bf421201aef872dff4
John Owen <johnowen77777@gmail.com>
2023-09-06 04:43:28 +0700

    Membuat module golang dasar

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/johnowen04/golang-dasar
+
+go 1.21.0
---
+++ helloworld.go
@@ -0,0 +1,9 @@
+package golang_dasar
+
+func SayHello() string {
+	return "Hello!"
+}
+
+func SayHelloWorld() string {
+	return "Hello! World"
+}
